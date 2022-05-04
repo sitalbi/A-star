cbe80d68e80dace2aed4e85920f2227919a91b8a
sitalbi <simon0111@live.fr>
2022-05-04 23:57:36 +0200

    creating Grid

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid {
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+
+    public Grid(int width, int height, float cellSize) {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.gridArray = new int[width,height];
+        
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+
+
+    private Vector3 GetWorldPosition(int x, int y) {
+        return new Vector3(x, y) * cellSize;
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid grid = new Grid(20, 10, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
