506367fd3523d58dfa68cb67794b793b347865cb
sitalbi <simon0111@live.fr>
2022-05-15 22:02:34 +0200

    Heap class fully implemented

--- Assets/Scripts/Heap.cs
+++ Assets/Scripts/Heap.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Heap
 {
-    public Node[] array; // array begins at index 1
-    public int n, nmax;
+    private Node[] array; // array begins at index 1
+    private int n, nmax; // n: number of Node in the heap; nmax: maximum of Node that can be added to the heap
     
     public Heap(int nmax) {
         this.n = 0;
@@ -15,8 +15,58 @@ public class Heap
     public int ComparisonFunction(Node a, Node b) {
         return a.score - b.score;
     }
+    
+    /**
+     *  Add a Node to the heap
+    */
+    public void HeapAdd(Node node) {
+        if(this.n<this.nmax) {
+            this.n++;
+            array[n] = node;
 
-    public void HeapAdd(Node n) {
-        // TODO
+            int k = n;
+            while (k>1 && ComparisonFunction(node, array[((int)(k/2))]) < 0) {
+                //swap
+                Node tmp = array[k];
+                array[k] = array[k/2];
+                array[k/2] = tmp;
+                k = k/2;
+            }
+        }
+    }
+
+    private Node GetHeapTop() {
+        return array[1];
+    }
+
+    /**
+     * Return the root and remove it from the heap
+    */ 
+    public Node HeapPop() {
+        Node top = GetHeapTop();
+        array[1] = array[this.n];
+        this.n--;
+        int i = 1;
+        while (i<(n/2)+1) {
+            Node tmp = array[i];
+            //swap
+            if (ComparisonFunction(array[2 * i], array[(2 * i) + 1]) < 0) {
+                array[i] = array[i * 2];
+                array[i * 2] = tmp;
+                i *= 2;
+            } else {
+                array[i] = array[(i * 2) + 1];
+                array[(i * 2) + 1] = tmp;
+                i = (i * 2) + 1;
+            }
+        }
+        return top;
+    }
+
+    /**
+     * Check if the heap is empty 
+    */
+    public bool HeapIsEmpty() {
+        return n == 0;
     }
 }
